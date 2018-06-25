a435c1b06d46df7c579e366fbcbba1137b337fb5
mmheller <mm_heller@yahoo.com>
2018-06-25 11:18:46 -0600

    CED minor fix

--- testCED/extras/MH_SetupSummaryMap.js
+++ testCED/extras/MH_SetupSummaryMap.js
@@ -142,10 +142,15 @@ define([
               //var strDefQuery2 = localStorage.getItem("ls_strDefQuery2");
               
 
-              var str4Replacing = "(((SourceFeatureType = 'point') OR ( SourceFeatureType = 'poly' AND Wobbled_GIS = 1)) and (TypeAct not in ('Non-Spatial Plan', 'Non-Spatial Project')))";
+              var str4Replacing = "and (((SourceFeatureType = 'point') OR ( SourceFeatureType = 'poly' AND Wobbled_GIS = 1)) and (TypeAct not in ('Non-Spatial Plan', 'Non-Spatial Project')))";
               strDefQuery4LinePoly = strDefQuery;
               strDefQuery4LinePoly = strDefQuery4LinePoly.replace(str4Replacing, "");
-              str4Replacing = "((SourceFeatureType = 'point') OR ( SourceFeatureType = 'poly' AND Wobbled_GIS = 1)) and (TypeAct not in ('Non-Spatial Plan', 'Non-Spatial Project'))";
+
+              str4Replacing = "(((SourceFeatureType = 'point') OR ( SourceFeatureType = 'poly' AND Wobbled_GIS = 1)) and (TypeAct not in ('Non-Spatial Plan', 'Non-Spatial Project')))";
+              strDefQuery4LinePoly = strDefQuery;
+              strDefQuery4LinePoly = strDefQuery4LinePoly.replace(str4Replacing, "");
+
+              str4Replacing = " and ((SourceFeatureType = 'point') OR ( SourceFeatureType = 'poly' AND Wobbled_GIS = 1)) and (TypeAct not in ('Non-Spatial Plan', 'Non-Spatial Project'))";
               strDefQuery4LinePoly = strDefQuery4LinePoly.replace(str4Replacing, "");
 
               var strBase_URL = "https://services.arcgis.com/QVENGdaPbd4LUkLV/arcgis/rest/services/CED_Base_Layers/FeatureServer/"
