365f7cce230528341cf906ff5691725d1415c299
mmheller <mmheller@users.noreply.github.com>
2019-08-01 13:56:14 -0600

    added SRU web tool

---
+++ CED_SRU_WebEditor/extras/MH_SRUEdit_Setup.js
@@ -0,0 +1,247 @@
+﻿//Created By:  Matt Heller,  U.S. Fish and Wildlife Service, Science Applications, Region 6
+//Date:        May 2018, Updated May 2019
+
+
+function showLoading() {
+    esri.show(app.loading);
+    app.map.disableMapNavigation();
+    app.map.hideZoomSlider();
+}
+
+function hideLoading(error) {
+    esri.hide(app.loading);
+    app.map.enableMapNavigation();
+    app.map.showZoomSlider();
+}
+
+
+
+define([
+	"esri/Color", "esri/renderers/SimpleRenderer",
+	"esri/layers/LabelLayer",
+	"esri/symbols/TextSymbol", "esri/tasks/query",
+	"esri/toolbars/draw",
+	"esri/symbols/SimpleFillSymbol",
+	"esri/symbols/SimpleLineSymbol",
+    "dijit/form/CheckBox", "esri/layers/GraphicsLayer",
+    "esri/dijit/BasemapGallery",
+    "esri/geometry/webMercatorUtils",
+    "dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/_base/array",
+    "esri/urlUtils",
+    "esri/layers/FeatureLayer",
+    "esri/dijit/Scalebar",
+    "esri/geometry/scaleUtils", "dojo/_base/array", 
+    "esri/layers/FeatureLayer",
+    "dojo/dom",
+    "dojo/dom-class",
+    "dijit/registry",
+    "dojo/mouse",
+    "dojo/on",
+	"esri/map",
+	"extras/MH_SRUPopUniqueQueryInterfaceValues"
+], function (Color, SimpleRenderer, LabelLayer, TextSymbol, Query, Draw, SimpleFillSymbol, SimpleLineSymbol,
+		CheckBox, GraphicsLayer, BasemapGallery, webMercatorUtils, declare, lang, arrayUtil, urlUtils, FeatureLayer,
+            Scalebar, scaleUtils, arrayUtils, FeatureLayer,
+			dom, domClass, registry, mouse, on, Map, MH_SRUPopUniqueQueryInterfaceValues) {
+
+    return declare([], {
+		Phase1: function () {
+			$(function () {
+				$('#cbx_SRU1').change(function () {
+					$('#DivInitialCBXs2').toggle(this.checked);
+				}).change(); //ensure visible state matches initially
+			});
+
+			$(function () {
+				$('#cbx_SRU2').change(function () {
+					$('#dropdownForm').toggle(this.checked);
+				}).change(); //ensure visible state matches initially
+			});
+
+			$(function () {
+				$('#cbx_SRU3').change(function () {
+					$('#MapPickerArea').toggle(this.checked);
+
+				}).change(); //ensure visible state matches initially
+			});
+
+			//on(dom.byId("ddlSRU"), "change", ddlMatrix_Change);
+			on(dom.byId("ddlSRUState"), "change", ddlMatrix_Change);
+			on(dom.byId("ddlBLMHAF"), "change", ddlMatrix_Change);
+
+			app.strSRU_URL = "https://services.arcgis.com/QVENGdaPbd4LUkLV/ArcGIS/rest/services/SRU_HAF_Map/FeatureServer/0";
+			app.MH_SRUUniques = new MH_SRUPopUniqueQueryInterfaceValues({ strURL: app.strSRU_URL, iNonSpatialTableIndex: 0, divTagSource: null });
+			//app.MH_SRUUniques = new MH_SRUPopUniqueQueryInterfaceValues({ });
+
+			app.iNonSpatialTableIndex = 0;  //
+			app.MH_SRUUniques.divTagSource = null;
+			app.MH_SRUUniques.qry_SetUniqueValuesOf("Name", "SRU_ID", "Image", document.getElementById("ddlSRU"), "OBJECTID_1  > 0"); //maybe move this to MH_FeatureCount  //clear111
+
+			app.pSup.Phase2();
+
+			function ddlMatrix_Change(divTagSource) {
+				$('#ddlSRU').ddslick('destroy');
+				
+
+				document.getElementById("loadingImg").style.visibility = "visible";
+				disableOrEnableFormElements("dropdownForm", 'select-one', true); //disable/enable to avoid user clicking query options during pending queries
+				disableOrEnableFormElements("dropdownForm", 'button', true);  //disable/enable to avoid user clicking query options during pending queries
+
+				//app.map.infoWindow.hide();
+				//app.map.graphics.clear();
+				//CED_PP_poly.clearSelection();
+
+				//var strddlSRU = document.getElementById("ddlSRU").options[document.getElementById("ddlSRU").selectedIndex].value;
+				var strddlSRUState = document.getElementById("ddlSRUState").options[document.getElementById("ddlSRUState").selectedIndex].value;
+				var strddlBLMHAF = document.getElementById("ddlBLMHAF").options[document.getElementById("ddlBLMHAF").selectedIndex].value;
+
+				//if (((strddlSRU == "All") | (strddlSRU == "99")) &
+				//	(strddlSRUState == "99") &
+				//	(strBLMstrddlBLMHAFHAF == "99")) {
+				//	//dojo.forEach(app.arrayLayers, function (player) {
+				//	//	var id = player.id;
+				//	//	if (id == "graphicsLayer1") { player.setVisibility(!blnTPKs); }
+				//	//});
+				//}
+				//else {
+				//	//dojo.forEach(app.arrayLayers, function (player) {
+				//	//	var id = player.id;
+				//	//	if (id == "graphicsLayer1") { player.setVisibility(false); }
+				//	//});
+				//}
+
+				app.strQueryLabelText = "";
+
+				var strQuery = "";
+				//if ((app.arrayPrjIDs_fromSpatialSelect != undefined) & (app.arrayPrjIDs_fromSpatialSelect != "")) {
+				//	strQuery = "project_ID in (";
+				//	for (var i = 0; i < app.arrayPrjIDs_fromSpatialSelect.length; i++) {
+				//		strQuery += app.arrayPrjIDs_fromSpatialSelect[i] + ",";
+				//	}
+				//	strQuery = strQuery.slice(0, -1) + ")";
+				//}
+
+				if (strddlSRUState !== "99") {
+					if (strQuery !== "") { strQuery += " and "; }
+					strQuery += "State = '" + strddlSRUState + "'";
+					//app.strQueryLabelText += "Entry Type = " + document.getElementById("ddlEntry").options[document.getElementById("ddlEntry").selectedIndex].text + ", ";
+				}
+
+				if (strddlBLMHAF !== "99") {
+					if (strQuery !== "") { strQuery += " and "; }
+					strQuery += "HAF_SRU = '" + strddlBLMHAF + "'";
+					//app.strQueryLabelText += "Start Year = " + document.getElementById("ddlStartYear").options[document.getElementById("ddlStartYear").selectedIndex].text + ", ";
+				}
+
+				ExecutetheDerivedQuery(strQuery, divTagSource);
+
+
+
+			};
+
+			function ExecutetheDerivedQuery(strQuery, divTagSource) {
+				if (strQuery == "") { strQuery = "OBJECTID_1 > 0"; }
+				app.iNonSpatialTableIndex = 0;  //
+
+				app.MH_SRUUniques.divTagSource = divTagSource;
+				app.MH_SRUUniques.qry_SetUniqueValuesOf("Name", "SRU_ID", "Image", document.getElementById("ddlSRU"), strQuery); //maybe move this to MH_FeatureCount  //clear111
+				
+				app.pSRULayer.setDefinitionExpression(strQuery);
+
+				//if (document.getElementById("cbx_zoom").checked) {
+				//	var pZoom2 = new MH_Zoom2FeatureLayer({ pMap: app.map, dblExpandNum: 1.0 }); // instantiate the zoom class
+				//	var pZoom2Result = pZoom2.qry_Zoom2FeatureLayerExtent(this.pCED_PP_point);
+				//}
+			};
+
+			function setQS(strQueryDef) {
+				this.pCED_PP_poly.setDefinitionExpression(strQueryDef);
+				this.pCED_PP_poly.setVisibility(true);
+			};
+
+        },
+
+		Phase2: function () {
+			//app.loading = dojo.byId("loadingImg");  //loading image. id
+			var customExtentAndSR = new esri.geometry.Extent(-14000000, 4800000, -11000000, 6200000, new esri.SpatialReference({ "wkid": 3857 }));
+			app.map = new esri.Map("map", { basemap: "topo", logo: false, extent: customExtentAndSR });
+
+
+			var strlabelField1 = "Name";
+			var strlabelField2 = "SRU_ID";
+
+			//var selectionToolbar;
+			app.map.on("load", initSelectToolbar);
+
+			app.pSRULayer = new FeatureLayer(app.strSRU_URL, { id: "0", mode: FeatureLayer.MODE_ONDEMAND, outFields: [strlabelField1, strlabelField2], visible: true });
+
+			var pSelectionSymbol =
+				new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+					new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT,
+						new Color([255, 0, 0]), 2), new Color([255, 255, 0, 0.5]));
+
+			app.pSRULayer.setSelectionSymbol(pSelectionSymbol);
+			app.pSRULayer.on("selection-complete", PointSelectClick);
+			//pSRULayer.on("selection-clear", function () {
+			//	dom.byId('messages').innerHTML = "<i>SRU Not Selected</i>";
+			//});
+
+
+			var vRedColor = new Color("#FF0000");              // create a text symbol to define the style of labels
+			var pLabel1 = new TextSymbol().setColor(vRedColor);
+			pLabel1.font.setSize("8pt");
+			pLabel1.font.setFamily("helvetica");
+			var pLabelRenderer1 = new SimpleRenderer(pLabel1);
+			var plabels1 = new LabelLayer({ id: "labels1" });
+			plabels1.addFeatureLayer(app.pSRULayer, pLabelRenderer1, "{" + strlabelField1 + "} : {" + strlabelField2 + "}");
+
+			dojo.connect(app.map, "onUpdateStart", showLoading);
+			dojo.connect(app.map, "onUpdateEnd", hideLoading);
+
+			app.map.addLayers([app.pSRULayer, plabels1]);
+
+			function initSelectToolbar(event) {
+				app.selectionToolbar = new Draw(event.map);
+				var selectQuery = new Query();
+				app.selectionToolbar.activate(Draw.POINT);
+				on(app.selectionToolbar, "DrawEnd", function (geometry) {
+					app.selectionToolbar.deactivate();
+					selectQuery.geometry = geometry;
+					app.pSRULayer.selectFeatures(selectQuery,
+						app.pSRULayer.SELECTION_NEW);
+				});
+			}
+
+			function PointSelectClick(event) {
+				if (event.features.length == 0) {
+					dom.byId('messages').innerHTML = "<b>Nothing selected</b>";
+				}
+				else {
+					arrayUtil.forEach(event.features, function (feature) {
+						var iSRU_ID = feature.attributes.SRU_ID;
+						for (var ii = 0; ii < app.MH_SRUUniques.ddDataSRU.length; ii++) {
+							dataElement = app.MH_SRUUniques.ddDataSRU[ii];
+							if (dataElement.value == iSRU_ID) {
+								$('#ddlSRU').ddslick('select', { index: ii });
+							}
+						}
+						dom.byId('messages').innerHTML = "";
+					});
+				}
+				app.selectionToolbar.activate(Draw.POINT);
+			}
+		},
+
+		ddSRUslickSelected: function (iSRU_ID) {
+			alert("test, SRU_ID selected " + iSRU_ID.toString());
+		},
+
+        err: function (err) {
+            console.log("Failed to get stat results due to an error: ", err);
+        }
+
+    });
+  }
+);
---
+++ CED_SRU_WebEditor/extras/MH_SRUPopUniqueQueryInterfaceValues.js
@@ -0,0 +1,342 @@
+﻿////Created By:  Matt Heller,  U.S. Fish and Wildlife Service, Science Applications, Region 6
+//Date:        Oct 2014, Updated Oct 2018
+
+function disableOrEnableFormElements(strFormName, strElementType, TorF) {
+    var pform = document.getElementById(strFormName);   // enable all the dropdown menu's while queries are running
+
+    if (pform != null) {
+        for (var i = 0; i < pform.elements.length; i++) {
+            if (pform.elements[i].type == strElementType) {
+                strID = pform.elements[i].id;
+                document.getElementById(strID).disabled = TorF;
+            }
+        }
+    }
+
+}  
+
+define([
+  "dojo/_base/declare",
+  "dojo/_base/lang",
+  "esri/request"
+], function (
+  declare, lang, esriRequest
+) {
+
+    function trimStringWhiteSpace(str) {
+        return str.replace('     ', ' ').replace('    ', ' ').replace('  ', ' ');
+    }
+
+    function sortFunction(a, b) {
+        if (isNaN(a.T)) {
+            var textA = a.T.toUpperCase();
+            var textB = b.T.toUpperCase();
+        }
+        else {
+            var textA = a.T;
+            var textB = b.T;
+        }
+
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    }
+
+    return declare([], {
+        strURL: null,
+        strContaminant: null,
+        strMatrix: null,
+        strManagUnit: null,
+        strDataType: null,
+        iNonSpatialTableIndex: 0,
+		divTag4Results: null,
+		ddDataSRU: null,
+        strFieldNameText: null,
+        strFieldNameValue: null,
+        strQuery1: null,
+        strQuery2: null,
+        divTagSource: null,
+        numberOfErrors: 0,
+
+        constructor: function (options) {            // specify class defaults
+            this.strURL = options.strURL || "test";
+            this.strContaminant = options.strContaminant || "20mi"; // default seat geek range is 30mi
+            this.strMatrix = options.strMatrix || ""; // default to 50 results per page
+            this.strManagUnit = options.strManagUnit || "";
+            this.strDataType = options.strDataType || "";
+            this.iNonSpatialTableIndex = options.iNonSpatialTableIndex || 0;
+            this.divTag4Results = options.divTag4Results || null;
+            this.returnEvents = lang.hitch(this, this.returnEvents);
+            this.returnEvents2 = lang.hitch(this, this.returnEvents2);
+            this.strQuery1 = options.strQuery1 || "";
+            this.divTagSource = options.divTagSource || "";
+            this.m_strCED_PP_pointQuery = null;
+        },
+
+
+        qry_SetUniqueValuesOf: function (strFieldNameText, strFieldNameValue, strFieldImageName, divTag4Results, strQuery) {
+            this.divTag4Results = divTag4Results;
+            this.strFieldNameText = strFieldNameText;
+			this.strFieldNameValue = strFieldNameValue;
+			this.strFieldImageName = strFieldImageName;
+
+			arraystrFieldNames = [strFieldNameText, strFieldNameValue];
+			if (strFieldImageName != "") {
+				arraystrFieldNames.push(strFieldImageName);
+			}
+            this.strQuery1 = strQuery;
+
+            var pQueryT = new esri.tasks.QueryTask(this.strURL + this.iNonSpatialTableIndex + "?returnDistinctValues=true");
+            var pQuery = new esri.tasks.Query();
+            pQuery.returnGeometry = false;
+			pQuery.outFields = arraystrFieldNames;
+            pQuery.where = strQuery;
+            return pQueryT.execute(pQuery, this.returnEvents, this.err);
+        },
+
+
+        returnEvents: function (results) {
+            var strRemoveStrings = ["", "---select a SRU filter---"];
+            var resultFeatures = results.features;
+            var strdivTagSourceID = "";
+            var strText = "";
+
+            if ((this.divTagSource != "") & (this.divTagSource != null)) {
+                strdivTagSourceID = this.divTagSource.target.id;
+            }
+            
+            var strdivTag4ResultsID = "";
+            if (this.divTag4Results != null) {
+                strdivTag4ResultsID = this.divTag4Results.id;
+            }
+            
+
+            if ((resultFeatures != null) || (resultFeatures != undefined)) {
+                if (resultFeatures.length > 0) {
+                    var featAttrs = resultFeatures[0].attributes;
+                    var values = [];
+					var texts = [];
+					var imagesSRU = [];
+					var strText = "";
+					var iValue = "";
+					var strImagePath = "";
+                    var strValue = "";
+                    var testTexts = {};
+                    var attrNames = [];
+                    var blnAdd2Dropdown;
+                    for (var i in featAttrs) { attrNames.push(i); }
+
+                    dojo.forEach(resultFeatures, function (feature) {//Loop through the QueryTask results and populate an array with the unique values
+                        blnAdd2Dropdown = false;
+                        dojo.forEach(attrNames, function (an) {
+							if (an.toLowerCase() == this.app.MH_SRUUniques.strFieldNameText.toString().toLowerCase()) {
+                                var strTempText = feature.attributes[an];
+                                if (!testTexts[strTempText]) {
+                                    testTexts[strTempText] = true;
+
+                                    strText = strTempText;
+                                    if ((strText == null) || (strText == undefined)) {
+                                        strText = "null or undefined";
+                                    }
+                                    blnAdd2Dropdown = true;
+                                    dojo.forEach(strRemoveStrings, function (str2remove) {  //check to see if should add to the values for the dropdown list
+                                        if (strText.toString != undefined)   {
+                                            if (isNaN(strText)){
+                                                if (strText.toString() == "null or undefined") {
+                                                    blnAdd2Dropdown = false;
+                                                }
+                                                else if (str2remove.toLowerCase() == strText.toString().toLowerCase()) {
+                                                    blnAdd2Dropdown = false;
+                                                }
+                                            }
+                                        }
+                                        else { console.log("error with: if (strValue.toString != undefined) {"); }
+                                    });
+                                }
+                            }
+							if (an.toLowerCase() == this.app.MH_SRUUniques.strFieldNameValue.toString().toLowerCase()) {
+                                iValue = feature.attributes[an];
+							}
+							if (an.toLowerCase() == this.app.MH_SRUUniques.strFieldImageName.toString().toLowerCase()) {
+								strImagePath = "images/" + feature.attributes[an];
+							}
+                        });
+                        if (blnAdd2Dropdown) {
+							if (strText == "") { strText = iValue.toString(); }
+                            texts.push(strText);
+							values.push(iValue);
+							if (this.app.MH_SRUUniques.strFieldImageName != "") {
+								imagesSRU.push(strImagePath);
+							}
+                        } //values.push({ name: zone });
+                        strText = "";
+						iValue = "";
+						strImagePath = "";
+                    });
+
+                    if (this.strFieldNameText == "Start_Year") {
+                        var all = [];
+                        for (var i = 0; i < values.length; i++) {
+                            all.push({ 'T': texts[i], 'V': values[i] });
+                        }
+                        if (this.strFieldNameText == "ST_ID") {
+                            var temp = "";
+                        }
+                        all.sort(sortFunction);
+                        texts = [];
+                        values = [];
+                        arrayOfNot2ShowActivityValues = ["1900", "1940", "1947", "1960",
+                                                         "1973", "1977", "1978", "1979",
+                                                         "1980", "1981", "1983", "1984",
+                                                         "1985", "1986", "1987", "1988", "1989",
+                                                         "1990", "2095", "2098", "2099"];
+                        //arrayOfNot2ShowActivityValues = [""];
+
+                        for (var i = 0; i < all.length; i++) {
+                            blnShow = true;
+                            dojo.forEach(arrayOfNot2ShowActivityValues, function (arrayOfNot2ShowActivityValue) {
+                                if ((arrayOfNot2ShowActivityValue == all[i].T) | (arrayOfNot2ShowActivityValue == all[i].V)) {
+                                    blnShow = false;
+                                }
+                            });
+                            if (blnShow) {
+                                texts.push(all[i].T);
+                                values.push(all[i].V);
+                            }
+                        }
+                    }
+                    else if (this.strFieldNameText != "Project_ID") {
+                        var all = [];
+                        for (var i = 0; i < values.length; i++) {
+							if (this.strFieldImageName != "") {
+								all.push({ 'T': texts[i], 'V': values[i], 'I': imagesSRU[i] });
+							} else {
+								all.push({ 'T': texts[i], 'V': values[i] });
+							}
+                        }
+                        if (this.strFieldNameText == "ST_ID") {
+                            var temp = "";
+                        }
+                        all.sort(sortFunction);
+                        texts = [];
+                        values = [];
+						imagesSRU = [];
+
+                        for (var i = 0; i < all.length; i++) {
+                            texts.push(all[i].T);
+							values.push(all[i].V);
+							if (this.strFieldImageName != "") {
+								imagesSRU.push(all[i].I);
+							}
+                        }
+                    } 
+                }
+
+				
+
+                if ((strdivTag4ResultsID != "") & (strdivTag4ResultsID != strdivTagSourceID)) {
+					if (this.divTag4Results.id != "ddlSRU") {
+						var strTempValue = this.divTag4Results.options[this.divTag4Results.selectedIndex].text;                //record the existing selection
+						this.divTag4Results.options.length = 0; // clear out existing items
+
+						var opt = new Option("All", 99);
+						this.divTag4Results.options.add(opt)
+						this.divTag4Results.selectedIndex = 0
+					} else {
+						this.ddDataSRU = [];
+					}
+
+					if (values != undefined) {
+                        for (var i = 0; i < values.length; i++) {
+                            iValue = values[i];
+
+                            var tt = texts[i];
+                            if (this.divTag4Results.id == "ddlEntry") {
+                                var strDTextTemp = tt.toString();
+                            }
+
+							if (this.divTag4Results.id == "ddlSRU") {
+								this.ddDataSRU.push({
+									text: tt,
+									value: iValue,
+									selected: false,
+									description: "SRU ID: " + iValue,
+									imageSrc: imagesSRU[i]
+												});
+							}
+
+							if (this.divTag4Results.id != "ddlSRU") {
+								var opt = new Option(tt, iValue);
+								this.divTag4Results.options.add(opt);
+							}
+							if ((strTempValue == tt) & (strdivTagSourceID != "")) {
+                                this.divTag4Results.selectedIndex = i + 1
+                            }
+                        }
+					}
+                }
+                else if (this.strFieldNameText == "Project_ID") {
+                    if (values == undefined) {
+                        var strstop = ""; 
+                    }
+
+					if (this.strQuery1 == "OBJECTID_1 > 0") {
+                        //do nothing
+                    } else {
+                        var strQuery2 = "Project_ID in (";
+                        for (var i = 0; i < values.length; i++) { strQuery2 += values[i] + ","; }
+                        this.strQuery1 = strQuery2.slice(0, -1) + ")";
+                    }
+                }
+            }
+
+
+
+			if (this.divTag4Results.id == "ddlSRU") {
+				var temp = this.ddDataSRU;
+
+				$('#ddlSRU').ddslick({
+					data: this.ddDataSRU,
+					width: 300,
+					selectText: "Select your preferred SRU (Sagebrush Reporting Unit)",
+					imagePosition: "right",
+					onSelected: function (pData) {
+						
+						app.pSup.ddSRUslickSelected(pData.selectedData.value);
+					}
+				});
+			}
+
+            switch (this.strFieldNameText) {                //                'count' | 'sum' | 'min' | 'max' | 'avg' | 'stddev'
+				case "Name":
+					this.qry_SetUniqueValuesOf("State", "State", "", document.getElementById("ddlSRUState"), this.strQuery1);
+                    break;
+				case "State":
+					this.qry_SetUniqueValuesOf("HAF_SRU", "HAF_SRU", "", document.getElementById("ddlBLMHAF"), this.strQuery1);
+                    break;
+
+				case "HAF_SRU":
+					document.getElementById("loadingImg").style.visibility = "hidden";
+					disableOrEnableFormElements("dropdownForm", 'select-one', false); //disable/enable to avoid user clicking query options during pending queries
+					disableOrEnableFormElements("dropdownForm", 'button', false);  //disable/enable to avoid user clicking query options during pending queries
+					this.iNonSpatialTableIndex = 0; //reset the table index for next time
+
+					break;
+            }
+
+            return results;
+        },
+
+        err: function (err) {
+            $(function () {
+                $("#dialogWarning1").dialog("open");
+            });
+			console.log("Error number" + String(this.app.MH_SRUUniques.numberOfErrors) + " Failed to get stat results due to an error: " + this.app.MH_SRUUniques.strFieldNameText + this.app.MH_SRUUniques.strFieldNameValue + " " + this.app.MH_SRUUniques.strURL + this.app.iNonSpatialTableIndex, err);
+			this.app.MH_SRUUniques.numberOfErrors += 1;
+            disableOrEnableFormElements("dropdownForm", 'select-one', false); //disable/enable to avoid user clicking query options during pending queries
+            disableOrEnableFormElements("dropdownForm", 'button', false);  //disable/enable to avoid user clicking query options during pending queries
+
+
+        }
+    });
+}
+);
+
