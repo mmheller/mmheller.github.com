a05008aea8c9bfb72048744473fd4a88f90fe7b4
mmheller <matthew_heller@fws.gov>
2025-04-09 10:52:12 -0600

    github pages cleanup

---
+++ PTSR6SA/extras/CK_AutoCompleteSearchAndZoom.js
@@ -0,0 +1,168 @@
+﻿//Created By:  Matt Heller, Great Northern Landscape Conservation Cooperative / U.S. Fish and Wildlife Service
+//Date:        Oct 2014
+
+define([
+  "dojo/_base/declare",
+  "dojo/_base/lang",
+  "esri/request",
+  "esri/tasks/FindTask",
+   "esri/tasks/FindParameters",
+	"esri/tasks/query",
+	"dojo/promise/all",
+], function (
+  declare, lang, esriRequest, FindTask, FindParameters, query, All
+) {
+
+    return declare([], {
+        pMap: null,
+        dblExpandNum: null,
+        pFeatureLayer: null,
+        divTag4Results: null,
+        strSearchField: null,
+        pSR: null,
+        strURL: null,
+  
+        constructor: function (options) {
+            this.pMap = options.pMap || null;
+            this.dblExpandNum = options.dblExpandNum || null;
+            this.returnEvents = lang.hitch(this, this.returnEvents);            // returnEvents is called by an external function, esri.request hitch() is used to provide the proper context so that returnEvents will have access to the instance of this class
+            this.divTag4Results = options.divTag4Results || null;
+            this.strSearchField = options.strSearchField || null;
+            this.pSR = options.pSR || null;
+            this.strURL = options.strURL || null;
+        },
+
+        ExecutePTSFind: function (strSearchValue) { //had to revamp to a regular query due to AGOL not supporting find operations
+            app.gQuery.ClearDivs();
+
+			var q_Layer1 = new esri.tasks.Query();
+			var qt_Layer1 = new esri.tasks.QueryTask(this.strURL + "/0");
+			q_Layer1.where = "ProjectID like '%" + strSearchValue + "%'" +
+                                 " or Prj_Title like '%" + strSearchValue + "%'" +
+                                 " or PI_Org like '%" + strSearchValue + "%'" +
+                                 " or Partner_Organizaitons like '%" + strSearchValue + "%'" +
+                                 " or Subject_Keywords like '%" + strSearchValue + "%'" +
+                                 " or Location_Keywords like '%" + strSearchValue + "%'" +
+                                 " or LeadName_LastFirst like '%" + strSearchValue + "%'"
+			q_Layer1.outFields = ["ProjectID"];
+
+			var q_Layer2 = new esri.tasks.Query();
+			var qt_Layer2 = new esri.tasks.QueryTask(this.strURL + "/9");
+			q_Layer2.where = "PersonName like '%" + strSearchValue + "%'" +	" or GroupName like '%" + strSearchValue + "%'" 
+			q_Layer2.outFields = ["projectid"];
+
+			q_Layer1.returnGeometry = false;
+			pLayer1 = qt_Layer1.execute(q_Layer1);
+			q_Layer2.returnGeometry = false;
+			pLayer2 = qt_Layer2.execute(q_Layer2);
+
+			var pLayer1, pLayer2, pPromises;
+			pPromises = new All([pLayer1, pLayer2]);
+			pPromises.then(this.showResultsFromFind, this.err);
+        },
+
+        showResultsFromFind: function (results) {
+			var tempValue;
+			var arrayValues = [];
+			var testVals = {};
+
+			for (let i = 0; i < 2; i++) {
+				if ((results[i] != null) && (results[i] != undefined)) {
+					if (results[i].features.length > 0) {
+						dojo.forEach(results[i].features, function (pfeatureItem) {  //Loop through the QueryTask results and populate an array with the unique values
+							tempValue = pfeatureItem.attributes.ProjectID;
+							if (tempValue == undefined) {
+								tempValue = pfeatureItem.attributes.projectid;
+							}
+							if (!testVals[tempValue]) {
+								testVals[tempValue] = true;
+								arrayValues.push(tempValue);
+								this.app.gQuery.arryExtraPrjIDs4URLParam.push(tempValue);
+								//var CheckedValue = tempValue;
+								//arrayValues.push(CheckedValue);
+								//this.app.gQuery.arryExtraPrjIDs4URLParam.push(CheckedValue);
+							}
+							else {
+								console.log("Project ID Already Acconted For");
+								//do nothing
+							}
+						});
+					}
+				}
+			}
+			if (arrayValues.length > 0) {
+				this.app.gQuery.arrayProjectIDs = arrayValues;
+				this.app.gQuery.m_iarrayQueryIndex += 1; //increment the index value of the query array by 1
+				if (app.gQuery.strQuery == null) {
+					app.gQuery.strQuery = "ProjectID in (" + arrayValues.join(",") + ")";
+				} else {
+					app.gQuery.strQuery = app.gQuery.strQuery.replace(")", "," + arrayValues.join(",") + ")");
+				}
+				app.gQuery.SendQuery4ProjectResults(app.gQuery.strQuery, app.gQuery.m_grid);
+			}
+        },
+        
+        zoomToPoint: function (pointx, pointy, option, dblZoom) {
+            require(["esri/graphic", "esri/geometry/Point", "esri/SpatialReference"
+            ], function (Graphic, Point, SpatialReference) {
+                var ptempSR = new SpatialReference({ wkid: 3857 });
+
+                mapPoint1 = new Point(pointx, pointy, ptempSR);
+
+                if (option == "noPoint") { //just select the parcel so the perimeter is highlighted
+                    require(["esri/tasks/query", "esri/layers/FeatureLayer"], function (
+                        Query, FeatureLayer
+                ) {
+                        var query = new Query();
+                        query.geometry = mapPoint1;
+                        parcelLayer.selectFeatures(query, FeatureLayer.SELECTION_NEW, function (parcel) {
+                            var parcelObject = parcel[0];
+                            this.app.map.setExtent((map.graphics.add(new Graphic(createJSONPolygon(parcelObject.geometry.rings)))).geometry.getExtent().expand(1.5));
+                        });
+                    }); //End require
+                } else { //add a point graphic to the map and label it
+                    if (dblZoom == null) {
+                        this.app.map.centerAt(mapPoint1);
+                    }
+                    else {
+                        this.app.map.centerAndZoom(mapPoint1, dblZoom);
+                    }
+                }
+            }); //End zoomToPoint require
+        },
+
+			   		 
+        createJSONPolygon: function (coords, selector, atts) {
+            var frank = String(coords);
+
+            var fixedCoords = frank.replace(/,/g, "],[");
+            var fixedCoords2 = fixedCoords.replace(/],\[4/g, ",4");
+            fixedCoords2 = eval("[[[" + fixedCoords2 + "]]]")
+            map.graphics.clear();
+            PolyPost = {
+                "geometry": { "rings": fixedCoords2, "spatialReference": { "wkid": 102206} }, "symbol": {
+                    "color": [0, 0, 0, 64], "outline": { "color": [82, 246, 248, 255], "width": 2, "type": "esriSLS", "style": "esriSLSSolid" },
+                    "type": "esriSFS", "style": "esriSFSNull"
+                }
+            };
+            return PolyPost
+        },
+
+
+        returnEvents: function (results) {  //placeholder to enable return events if needed later
+            //            var resultFeatures = results.features;
+
+            return results;
+        },
+
+        err: function (err) {
+            console.log("Failed to get stat results due to an error: ", err);
+            alert(error.name);
+        }
+    }
+    )
+    ;
+
+}
+);
+
---
+++ PTSR6SA/extras/MH_Query4Results.js
@@ -0,0 +1,208 @@
+﻿//Created By:  Matt Heller, Great Northern Landscape Conservation Cooperative / U.S. Fish and Wildlife Service
+//Date:        Oct 2014
+function onRowClickHandler(evt) {
+    var strProjectID = evt.grid.getItem(evt.rowIndex).ProjectID.toString();
+    var strURL = "prj_report.html?PRJ_ID=" + strProjectID;
+    window.open(strURL);
+    //window.open(strURL, "_self");
+}
+
+
+define([
+  "dojo/_base/declare",
+  "dojo/_base/lang",
+  "esri/request",
+      "esri/IdentityManager",
+      "esri/layers/FeatureLayer",
+      "esri/dijit/FeatureTable",
+      "dojo/dom-construct",
+      "dojo/dom",
+      "dojo/parser",
+      "dojo/ready",
+      "dojo/on",
+      "dijit/registry",
+      "esri/tasks/query",
+      "dojox/grid/DataGrid",
+      "dojo/data/ItemFileReadStore",
+  ], function (
+      declare, lang, esriRequest, IdentityManager, FeatureLayer, FeatureTable,
+      domConstruct, dom, parser, ready, on,
+      registry, Query, DataGrid, ItemFileReadStore
+) {
+      return declare([], {
+          arrayProjectIDs: [],
+          strURL: null,
+          m_iarrayQueryIndex: null,
+          m_arrayQuery: null,
+          m_grid: null,
+          m_EventAdded2Grid: false,
+          strQuery: null,
+          m_arrayUserRemovedPrjs: [],
+          arryExtraPrjIDs4URLParam: [],
+          arryExtraPrjIDs4URLParam2Remove: [],
+          
+          constructor: function (options) {
+              this.strURL = options.strURL || "www.cnn.com"; // default AGS REST URL
+              //this.arrayProjectIDs = options.strURL || [];
+              this.arryExtraPrjIDs4URLParam = options.arryExtraPrjIDs4URLParam || []
+              this.m_arrayUserRemovedPrjs = options.m_arrayUserRemovedPrjs || [];
+
+          },
+
+          SendQuery2: function (arrayQuery, iarrayQueryIndex, pGrid) {
+            this.m_grid = pGrid;
+            this.m_arrayQuery = arrayQuery;
+            this.m_iarrayQueryIndex = iarrayQueryIndex;
+            if (iarrayQueryIndex == 0) { 
+                this.arrayProjectIDs = [];  //clear out the array if index is zero
+            }
+            pTblindexAndQuery = arrayQuery[iarrayQueryIndex];
+            var iIndex = pTblindexAndQuery[0];
+            var strQuery = pTblindexAndQuery[1];
+            var queryTask = new esri.tasks.QueryTask(this.strURL + "/" + iIndex.toString() + "?returnDistinctValues=true");
+            var pQuery = new Query();
+            pQuery.returnGeometry = false;
+            pQuery.outFields = ["ProjectID"];
+            //if (this.arrayProjectIDs.length > 0) {  !!!!!!!!!!!!!don't do this when handleing url parameters and the 1st of queries
+            //    strQuery = "(" + strQuery + ") and (ProjectID in (" + this.arrayProjectIDs.join(",") + "))";
+            //}
+            if (this.m_arrayUserRemovedPrjs.length > 0) {  //if user's right clicked and removed a row then don't include
+                strQuery += " and (not (ProjectID in (" + this.m_arrayUserRemovedPrjs.join(",") + ")))";
+            }
+
+            pQuery.where = strQuery;
+            queryTask.execute(pQuery, this.SendQueryResults, this.err);
+          },
+
+          SendQuery: function (arrayQuery, iarrayQueryIndex) {
+              if (arrayQuery.length > 0) {
+                  this.m_arrayQuery = arrayQuery;
+                  this.m_iarrayQueryIndex = iarrayQueryIndex;
+
+                  if (iarrayQueryIndex == 0) {
+                      this.arrayProjectIDs = [];  //clear out the array if index is zero
+                  }
+                  pTblindexAndQuery = arrayQuery[iarrayQueryIndex];
+                  var iIndex = pTblindexAndQuery[0];
+                  var strQuery = pTblindexAndQuery[1];
+                  var queryTask = new esri.tasks.QueryTask(this.strURL + "/" + iIndex.toString() + "?returnDistinctValues=true");
+                  var pQuery = new Query();
+                  pQuery.returnGeometry = false;
+                  pQuery.outFields = ["ProjectID"];
+
+                  if (this.arrayProjectIDs.length > 0) {
+                      strQuery = "(" + strQuery + ") and (ProjectID in (" + this.arrayProjectIDs.join(",") + "))";
+                  }
+                  if (this.m_arrayUserRemovedPrjs.length > 0) {  //if user's right clicked and removed a row then don't include
+                      strQuery += " and (not (ProjectID in (" + this.m_arrayUserRemovedPrjs.join(",") + ")))";
+                  }
+                  pQuery.where = strQuery;
+                  queryTask.execute(pQuery, this.SendQueryResults, this.err);
+              } else {  // if not query filters query all the values
+                  this.SendQuery4ProjectResults("OBJECTID > 0", this.m_grid);
+                  var tmp = "";
+              }
+          },
+
+          SendQueryResults: function (results) {
+              var resultFeatures = results.features;
+              if ((resultFeatures != null) && (resultFeatures != undefined)) {
+                  if (resultFeatures.length > 0) {
+                      var tempValue;
+                      var arrayValues = [];
+                      var testVals = {};
+
+                      dojo.forEach(resultFeatures, function (feature) {  //Loop through the QueryTask results and populate an array with the unique values
+                          tempValue = feature.attributes.ProjectID;
+                          if (tempValue == undefined) {
+                              tempValue = feature.attributes.projectid;
+                          }
+                          if (!testVals[tempValue]) {
+                              testVals[tempValue] = true;
+                              var CheckedValue = tempValue;
+                              arrayValues.push(CheckedValue); //values.push({ name: zone });//values.push("'" + strValue + "'"); //values.push({ name: zone });
+                          }
+                      });
+                      this.app.gQuery.arrayProjectIDs = arrayValues;
+                      this.app.gQuery.m_iarrayQueryIndex += 1; //increment the index value of the query array by 1
+
+                      if (this.app.gQuery.arryExtraPrjIDs4URLParam.length > 0) {   //////if id's passed via url then add for the query
+                          arrayValues = arrayValues.concat(this.app.gQuery.arryExtraPrjIDs4URLParam)
+                      }
+                      this.app.gQuery.strQuery = "ProjectID in (" + arrayValues.join(",") + ")";  //!!!!!!!!!!!!!!!!!!this gets used for the summary and the gis layer
+                      if (this.app.gQuery.m_iarrayQueryIndex >= this.app.gQuery.m_arrayQuery.length) {  //final query for projects
+                          this.app.gQuery.ClearDivs();
+                          this.app.gQuery.SendQuery4ProjectResults(this.app.gQuery.strQuery, this.app.gQuery.m_grid);  //reset the entire search page
+
+                      } else {
+                          this.app.gQuery.SendQuery(this.app.gQuery.m_arrayQuery, this.app.gQuery.m_iarrayQueryIndex);  //append the the existing search page projects
+                      }
+                  }
+              }
+              else {
+                  // do nothing
+              }
+          },
+
+          ClearDivs: function () {
+              document.getElementById('dTotalAllocatedbyLCC').innerHTML = "";
+              document.getElementById('dTotalAllocatedbyLCCbyYear').innerHTML = "";
+              document.getElementById('dTotalInKindMatchbyYear').innerHTML = "";
+              document.getElementById('dNumberOfFundingRecipients').innerHTML = "";
+              document.getElementById('dInKindFundingTypes').innerHTML = "";
+              document.getElementById('dFundRecipientTypes').innerHTML = "";
+              //              document.getElementById('dYearsFunded').innerHTML = "";
+              document.getElementById('dNumberOfProjectContacts').innerHTML = "";
+              document.getElementById('dTotalInKindMatch').innerHTML = "";
+              document.getElementById('dNumberofInKindOrgs').innerHTML = "";
+              document.getElementById('dTotalNumberofConsvTargets').innerHTML = "";
+              document.getElementById('dEcotypicAreas').innerHTML = "";
+              document.getElementById('dStressors').innerHTML = "";
+              document.getElementById('dGoals').innerHTML = "";
+              document.getElementById('dNumberofDeliverables').innerHTML = "";
+              document.getElementById('dDeliverabletype').innerHTML = "";
+              document.getElementById('dPrjStatus').innerHTML = "";
+          },
+
+
+          SendQuery4ProjectResults: function (strQuery, pGrid) {
+              this.ClearDivs();
+              document.getElementById("ImgResultsLoading").style.visibility = "visible";
+              this.m_grid = pGrid
+              var queryTask = new esri.tasks.QueryTask(this.strURL + "/0");
+              var pQuery = new Query();
+              pQuery.returnGeometry = false;
+              pQuery.outFields = ["ProjectID", "Prj_Title", "Fiscal_Years_of_Allocation", "PI_Org", "OBJECTID", "LeadName_LastFirst"];
+              pQuery.where = strQuery;
+              var items = [];
+              var gSup = app.gSup;
+              strURL = this.strURL;
+              var gQuerySummary = app.gQuerySummary;
+
+              if (!this.m_EventAdded2Grid) {  //if don't put this logic in then keeps on adding the click event to the data grid and gets repetitive
+                  pGrid.on("rowclick", onRowClickHandler);
+                  this.m_EventAdded2Grid = true;
+              }
+              dojo.connect(queryTask, "onComplete", function (featureSet) {
+                  var items = dojo.map(featureSet.features, function (feature) {                  //build an array of attributes
+                      return feature.attributes;
+                  });
+                  var data = { identifier: "OBJECTID", items: items };
+                  store = new ItemFileReadStore({ data: data });
+                  pGrid.setStore(store);
+                  gSup.Phase1(strURL, [], strQuery);
+                  gQuerySummary.Summarize(strQuery);
+              });
+              queryTask.execute(pQuery);
+          },
+          err: function (err) {
+              console.log("Failed to get stat results due to an error: ", err);
+              alert(error.name);
+          }
+      }
+    )
+    ;
+
+  }
+);
+
